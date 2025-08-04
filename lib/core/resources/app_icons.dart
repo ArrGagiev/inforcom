4e2e9a91027551e22be6289e62fb35ea71304166
Arrtorius <arrgagiev@gmail.com>
2025-08-04 20:14:11 +0300

    added all app icon

--- lib/core/resources/app_icons.dart
+++ lib/core/resources/app_icons.dart
@@ -17,6 +17,60 @@ class AppIcons {
   static const profileActive = '$_bottomIcons/profile_active_b_icon.svg';
   static const profileInactive = '$_bottomIcons/profile_b_icon.svg';
 
-  // Остальные иконки
-  // static const _icons = 'assets/icons';
+  // Основные иконки ---------------------------------------------------------
+  static const _icons = 'assets/icons';
+
+  static const backArrow = '$_icons/back_arrow_icon.svg';
+  static const basket = '$_icons/basket_icon.svg';
+  static const bathtub = '$_icons/bathtub_icon.svg';
+  static const bed = '$_icons/bed_icon.svg';
+  static const calendar = '$_icons/calendar_icon.svg';
+  static const car = '$_icons/car_icon.svg';
+  static const clock = '$_icons/clock_icon.svg';
+  static const close = '$_icons/close_icon.svg';
+  static const cup = '$_icons/cup_icon.svg';
+  static const filter = '$_icons/filter_icon.svg';
+  static const home = '$_icons/home_icon.svg';
+  static const hospital = '$_icons/hospital_icon.svg';
+  static const info = '$_icons/info_icon.svg';
+  static const iosRightArrow = '$_icons/ios_right_arrow_icon.svg';
+  static const location = '$_icons/location_icon.svg';
+  static const map = '$_icons/map_icon.svg';
+  static const minus = '$_icons/minus_icon.svg';
+  static const nearMe = '$_icons/near_me_icon.svg';
+  static const parking = '$_icons/parking_icon.svg';
+  static const plus = '$_icons/plus_icon.svg';
+  static const route = '$_icons/route_icon.svg';
+  static const sale = '$_icons/sale_icon.svg';
+  static const save = '$_icons/save_icon.svg';
+  static const search = '$_icons/search_icon.svg';
+  static const settings = '$_icons/settings_icon.svg';
+  static const shower = '$_icons/shower_icon.svg';
+  static const toilet = '$_icons/toilet_icon.svg';
+  static const trafficOff = '$_icons/traffic_off_icon.svg';
+  static const trafficOn = '$_icons/traffic_on_icon.svg';
+  static const user = '$_icons/user_icon.svg';
+  static const wc = '$_icons/wc_icon.svg';
+  static const web = '$_icons/web_icon.svg';
+
+  // Иконки для карты  -------------------------------------------------------
+  static const _mapIcons = 'assets/map_icons';
+
+  // Метки поумолчанию
+  static const mapDefault = '$_mapIcons/map_icon_default.svg';
+  static const mapDefaultM = '$_mapIcons/map_icon_default_m.svg';
+
+  // Средний тариф
+  static const mapAffordable = '$_mapIcons/map_icon_affordable.svg';
+  static const mapAffordableM = '$_mapIcons/map_icon_affordable_m.svg';
+
+  // Дешевый тариф
+  static const mapCheap = '$_mapIcons/map_icon_cheap.svg';
+  static const mapCheapM = '$_mapIcons/map_icon_cheap_m.svg';
+
+  // Дорогий тариф
+  static const mapExpensive = '$_mapIcons/map_icon_expensive.svg';
+  static const mapExpensiveM = '$_mapIcons/map_icon_expensive_m.svg';
+
+  static const mapPoint = '$_mapIcons/map_icon_point.svg';
 }
